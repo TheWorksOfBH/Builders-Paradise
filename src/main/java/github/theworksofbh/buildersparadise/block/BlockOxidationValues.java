27ca1622c38c4c148b804ac4072f5ef3182f42f8
BH <97546705+TheWorksOfBH@users.noreply.github.com>
2026-08-08 20:29:00 +0100

    Reworking the entire architecture of the mod, porting it to Multiloader

--- src/main/java/github/theworksofbh/buildersparadise/block/BlockOxidationValues.java
+++
@@ -1,6 +0,0 @@
-package github.theworksofbh.buildersparadise.block;
-
-public record BlockOxidationValues() {
-    public static final float IRON = 0.05296323F;
-    public static final float ZINC = 0.06347294F;
-}
--- src/main/java/github/theworksofbh/buildersparadise/mixins/FallingBlockEntityMixins.java
+++
@@ -1,8 +0,0 @@
-package github.theworksofbh.buildersparadise.mixins;
-
-import net.minecraft.world.entity.item.FallingBlockEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(FallingBlockEntity.class)
-public class FallingBlockEntityMixins {
-}
